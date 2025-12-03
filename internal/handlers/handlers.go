764b1706098e25746bce782b9760fcc0a60a609b
Eduard <stedi.str@gmail.com>
2025-12-03 19:26:05 +0300

    v.0.0.1 test

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,13 @@
 package handlers
+
+import "net/http"
+
+// для коня "/""
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// для "/upload"
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func CheckAndConvert(text string) string {
+	// проверяем на морзе удалив все точки, запятые, пробелы
+	str := strings.Trim(text, ".- ")
+
+	if str == "" {
+		// получили Азбуку морзе
+		return morse.ToText(text)
+	} else {
+		// получили текст
+		return morse.ToMorse(text)
+	}
+}
